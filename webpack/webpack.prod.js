2b95fcf5c745bb2d45956e4dedd6a76ca7c15956
hannut91 <hannut91@gmail.com>
2019-07-08 07:24:23 +0900

    Add asset compression
    Add asset compression with gzip

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,20 +1,23 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const MinifyPlugin = require("babel-minify-webpack-plugin");
-const ClosurePlugin = require('closure-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
   output: {
     path: __dirname + '/../dist',
     filename: 'main.js',
-    publicPath: 'https://yunseok.com',
+    publicPath: 'https://yunseok.com/',
   },
   plugins: [
     new MinifyPlugin({
       removeConsole: true,
-    })
+    }),
+    new CompressionPlugin({
+      deleteOriginalAssets: true,
+    }),
   ],
   optimization: {
     minimizer: [
